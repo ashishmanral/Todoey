db97b7b4e674a5e6da2752482eef89478cea874d
Ashish Manral <manral@amazon.com>
2018-04-20 11:24:34 -0700

    Persisting data using Codable

--- Todoey/Controller/TableViewController.swift
+++ Todoey/Controller/TableViewController.swift
@@ -10,14 +10,13 @@ import UIKit
 
 class TableViewController: UITableViewController {
     
-    var items = [Item(itemName : "Kela"), Item(itemName : "Chawal"), Item(itemName : "Pulao")]
-    let defaults = UserDefaults.standard
+    var items = [Item]()
+    let dataFilePath = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask)
+                            .first?.appendingPathComponent("Items.plist")
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        if let cachedItems = defaults.array(forKey: "items") as? [Item] {
-            items = cachedItems
-        }
+        decodeItemList()
     }
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
@@ -34,8 +33,9 @@ class TableViewController: UITableViewController {
 
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         items[indexPath.row].checked = items[indexPath.row].checked == false ? true : false
+        encodeItemList()
         tableView.cellForRow(at: indexPath)?.accessoryType = items[indexPath.row].checked ? .checkmark : .none
-        tableView.deselectRow(at: indexPath, animated: true)
+        //tableView.deselectRow(at: indexPath, animated: true)
     }
 
     @IBAction func addButtonPressed(_ sender: UIBarButtonItem) {
@@ -51,7 +51,7 @@ class TableViewController: UITableViewController {
                 if let text = textField.text {
                     if !text.isEmpty {
                         self.items.append(Item(itemName: text))
-                        self.defaults.set(self.items, forKey: "items")
+                        self.encodeItemList()
                         self.tableView.reloadData()
                     }
                 }
@@ -60,6 +60,28 @@ class TableViewController: UITableViewController {
         alertController.addAction(UIAlertAction(title: "Cancel", style: .cancel, handler: nil))
         self.present(alertController, animated: true, completion: nil)
         
+    }
+    
+    func encodeItemList() {
+        let encoder = PropertyListEncoder()
+        do {
+            let data = try encoder.encode(self.items)
+            try data.write(to : self.dataFilePath!)
+        } catch {
+            print("Error encoding")
+        }
+    }
+    
+    func decodeItemList() {
+        if let data = try? Data(contentsOf: dataFilePath!) {
+            let decoder = PropertyListDecoder()
+            do {
+                items = try decoder.decode([Item].self, from: data)
+            } catch {
+                print("Error decoding")
+            }
+            
+        }
     }
     
 }
--- Todoey/Model/Item.swift
+++ Todoey/Model/Item.swift
@@ -8,7 +8,7 @@
 
 import Foundation
 
-class Item {
+class Item : Codable {
     
     let itemName : String?
     var checked = false
